c24104b525dfa6dcaf4932c6f0acf3df0a02270f
Maia Cochran <maia.cochran1@gmail.com>
2022-11-09 18:21:46 -0700

    Add Header component

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,26 +1,14 @@
-// import React from 'react';
-// import { Sections } from '../data';
-// import { Link } from 'react-router-dom';
-// import './Header.css';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Header.css';
 
-// const Header = ({ handleChange }) => {
+const Header = () => {
+    const navigate = useNavigate();
+    return (
+        <div className="header">
+            <button className='home-page' onClick={() => navigate('/')}><h1>New York Times</h1></button>
+        </div>
+    )
+}
 
-//     const chooseSection = Sections.map(element => {
-//         return (<option key={element} value={element}>{element}</option>)
-//     })
-
-//     return (
-//         <div className="Nav">
-//             <Link to='/' className='TitleHome'><h1>New York Times</h1></Link>
-//             <label>
-//                 Select By:
-//                 <select onChange={event => handleChange(event)}>
-//                     <option>Choose a Value</option>
-//                     {chooseSection}
-//                 </select>
-//             </label>
-//         </div>
-//     )
-// }
-
-// export default Header;
+export default Header;
